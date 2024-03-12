cfd903ad855acd9a8e13d4575c41a126c78b6e6d
Emmanuel Akinbo <59098948+akinbo-emmanuel@users.noreply.github.com>
2024-03-12 15:25:23 +0100

    Change in skeleton design

--- src/components/SkeletonContainer.tsx
+++ src/components/SkeletonContainer.tsx
@@ -3,27 +3,27 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonContainer: FC = () => {
   return (
-    <div className=" grid grid-cols-5 gap-4 ">
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
-        <Skeleton className=" w-[15rem] h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
+        <Skeleton className=" w-auto h-[10rem] border border-gray-200 rounded p-4 mb-4" />
     </div>
   )
 }
